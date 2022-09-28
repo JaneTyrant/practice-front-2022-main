2cc2d58175b5483a9832b2b67bfd252f8b5a2731
TyrantJane <nonstop00066@gmail.com>
2022-09-28 04:33:12 +0300

    Refactor Header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -87,10 +87,10 @@ class Header extends React.Component {
             <a href="http://www.google.com">Read Announcement</a>
           </div>
           <div className={styles.loginSignnUpHeaders}>
-            <div className={styles.numberContainer}>
+            <a href={`tel:${CONSTANTS.CONTACTS.PHONE}`} className={styles.numberContainer}>
               <img src={`${CONSTANTS.STATIC_IMAGES_PATH}phone.png`} alt="phone" />
-              <span>(877)&nbsp;355-3585</span>
-            </div>
+              <span>{CONSTANTS.CONTACTS.PHONE}</span>
+            </a>
             <div className={styles.userButtonsContainer}>
               {this.renderLoginButtons()}
             </div>
@@ -100,6 +100,7 @@ class Header extends React.Component {
             <div className={styles.leftNav}>
               <div className={styles.nav}>
                 <ul>
+                  <li><Link to="/pricing"><span>PRICING</span></Link></li>
                   <li>
                     <span>NAME IDEAS</span>
                     <img
